53553d1d7184f566dade26f3cca158bcfd1fbe94
VanTej <vanessatejedor@gmail.com>
2022-11-16 12:15:41 +0100

    Init projet front

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+
+
+function App() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default App;
