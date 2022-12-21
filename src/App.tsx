841748933b91ef064c3b9fc5e69f5b7bcf8cf990
MariePerraud <m100chez@gmail.com>
2022-12-21 18:28:05 +0100

    création routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
-
+import Home from './pages/Home';
+import Catalog from './pages/Catalog';
+import Contact from './pages/Contact';
+import Profile from './pages/Profile';
+import Basket from './pages/Basket';
 
 function App() {
   return (
     <div>
-      <h1>WildRent</h1>
+      <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/catalogue" element={<Catalog />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/profil" element={<Profile />} />
+            <Route path="/panier" element={<Basket />} />
+          </Routes>
+        </Router>
     </div>
   );
 }
