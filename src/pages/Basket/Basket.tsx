370632a8c5acb69189ed8a874cc6b891020f2a64
MariePerraud <m100chez@gmail.com>
2023-01-26 16:47:57 +0100

    panier

--- src/pages/Basket/Basket.tsx
+++ src/pages/Basket/Basket.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import ProductBasket from '../../components/ProductBasket/ProductBasket';
+import ICatalogProps from '../../interfaces/ICatalogProps';
+import IProduct from '../../interfaces/IProduct';
+import "./Basket.css";
 
-function Basket() {
+function Basket({products}: ICatalogProps ) {
+  const [productsBasket, setProductsBasket] = useState<IProduct[]>([]);
+  
+  // On stock dans le state tous les produits au montage du composant
+  useEffect(() => {
+    setProductsBasket(products);
+  },[products]);
+  
   return (
-    <div>Mon panier</div>
+    <div className='basket_container'>
+      <h1 className='text-center mb-5'>Mon panier</h1>
+      <div className='row'>
+        <div>
+          {productsBasket.map((product) => (
+            <ProductBasket key={product.id} {...product} />
+          ))}
+        </div>
+      </div>
+    </div>
   )
 }
 
