262e11255d143fefe155abb1036e6bcd097f560c
VanTej <vanessatejedor@gmail.com>
2023-02-01 11:25:01 +0100

    list and delete categories

--- src/pages/Admin/AdminCustomers.tsx
+++ src/pages/Admin/AdminCustomers.tsx
@@ -1,6 +1,10 @@
 import "./admin.css";
 import AdminSidebar from "../../components/AdminSidebar/AdminSidebar";
-import { MdOutlineAddTask, MdOutlineDeleteOutline, MdOutlineModeEditOutline } from "react-icons/md";
+import {
+  MdOutlineAddTask,
+  MdOutlineDeleteOutline,
+  MdOutlineModeEditOutline,
+} from "react-icons/md";
 import { useEffect, useState } from "react";
 import { GET_ALL_CUSTOMERS } from "../../tools/queries";
 import IUser from "../../interfaces/IUser";
@@ -34,7 +38,11 @@ const AdminCustomers = () => {
   useEffect(() => {
     if (flashMessageMessage !== "") {
       setShowMessage(true);
-      const timer = setTimeout(() => setShowMessage(false), 4000);
+      const timer = setTimeout(() => {
+        setShowMessage(false);
+        setFlashMessageMessage("");
+        setFlashMessageType("");
+      }, 4000);
       return () => clearTimeout(timer);
     }
   }, [flashMessageMessage]);
@@ -53,37 +61,36 @@ const AdminCustomers = () => {
             <MdOutlineAddTask className="fs-1 me-2" />{" "}
             <span>Ajouter un client</span>
           </Button>
-
-          <Table striped hover>
-            <thead>
-              <tr>
-                <th className="text-center">#</th>
-                <th>Nom</th>
-                <th>Prénom</th>
-                <th>Email</th>
-                <th>Téléphone</th>
-                <th className="text-center">Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {customersAdmin &&
-                customersAdmin.map((customer) => (
-                  <tr key={customer.id}>
-                    <td className="text-center">{customer.id}</td>
-                    <td>{customer.lastname}</td>
-                    <td>{customer.firstname}</td>
-                    <td>{customer.email}</td>
-                    <td>{customer.phone}</td>
-                    <td className="d-flex justify-content-center align-items-center gap-2 flex-wrap">
-                      <MdOutlineModeEditOutline className="edit_icon fs-1" />
-                      <MdOutlineDeleteOutline
-                        className="delete_icon fs-1"
-                      />
-                    </td>
-                  </tr>
-                ))}
-            </tbody>
-          </Table>
+          <div className="w-75">
+            <Table striped hover>
+              <thead>
+                <tr>
+                  <th className="text-center">#</th>
+                  <th>Nom</th>
+                  <th>Prénom</th>
+                  <th>Email</th>
+                  <th>Téléphone</th>
+                  <th className="text-center">Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {customersAdmin &&
+                  customersAdmin.map((customer) => (
+                    <tr key={customer.id}>
+                      <td className="text-center">{customer.id}</td>
+                      <td>{customer.lastname}</td>
+                      <td>{customer.firstname}</td>
+                      <td>{customer.email}</td>
+                      <td>{customer.phone}</td>
+                      <td className="d-flex justify-content-center align-items-center gap-2 flex-wrap">
+                        <MdOutlineModeEditOutline className="edit_icon fs-1" />
+                        <MdOutlineDeleteOutline className="delete_icon fs-1" />
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+            </Table>
+          </div>
         </div>
       </div>
     </div>
