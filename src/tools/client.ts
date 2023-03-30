b59c1afff3b0fea9d347db1d4da0e7f97a48330d
VanTej <vanessatejedor@gmail.com>
2023-03-30 16:51:48 +0200

    https

--- src/tools/client.ts
+++ src/tools/client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  uri: "http://api.shamir6.wns.wilders.dev",
+  uri: "https://api.shamir6.wns.wilders.dev",
 });
 
 // Middleware pour intercepter
